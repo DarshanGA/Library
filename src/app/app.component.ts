c1baed0b2b9785f97c2b5a0ab816fc595f81eefb
DarshanGA <darshanjadav2016@gmail.com>
2025-08-31 20:20:24 +0530

    Added the nav bar component, its styles, offcanvas for smaller screens and its styles.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from "./navbar/navbar.component";
+
+import data from "../data/app-data.json";
 
 @Component({
   selector: 'app-root',
   //imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  imports: [NavbarComponent]
 })
 export class AppComponent {
   title = 'Library';
+  navOptions = data.navs;
+
 }
