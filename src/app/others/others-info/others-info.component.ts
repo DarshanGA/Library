e45429fe4d7228b022ecc452bed364b83a5a7b7f
DarshanGA <darshanjadav2016@gmail.com>
2025-10-22 10:25:40 +0530

    Added AI assets generation tool's related details under others page.

--- src/app/others/others-info/others-info.component.ts
+++ src/app/others/others-info/others-info.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { PageTitleComponent } from "../../common/page-title/page-title.component";
+import { SubTitleComponent } from "../../common/sub-title/sub-title.component";
+import { SubTitleNumbComponent } from "../../common/sub-title-numb/sub-title-numb.component";
+import { HyperLinkComponent } from "../../common/hyper-link/hyper-link.component";
 
 @Component({
   selector: 'app-others-info',
-  imports: [PageTitleComponent],
+  imports: [PageTitleComponent, SubTitleNumbComponent, HyperLinkComponent],
   templateUrl: './others-info.component.html',
   styleUrl: './others-info.component.scss'
 })
